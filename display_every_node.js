8868e01145e2c38a85b25a3b623a12560ff1d2fb
Sida Liu <learner.sida.liu@gmail.com>
2024-07-09 16:24:12 +0800

    all nodes except bugs

--- display_every_node.js
+++ display_every_node.js
@@ -1,18 +1,22 @@
 function addAllNodesToGraph() {
     let x = 50; // Starting x position
     let y = 50; // Starting y position
-    let deltaY = 150; // Y increment for each node
+    let deltaX = 250; // Y increment for each node
 
     for (let key in LiteGraph.registered_node_types) {
-        console.log(key);
-        const constantNode = LiteGraph.createNode(key);
-        constantNode.pos = [x, y];
-        graph.add(constantNode);
-        
-        y += deltaY; // Increment y position for next node
-        if (y > window.innerHeight - 50) {
-            y = 50;
-            x += 250; // Move to the next column
+        try {
+            console.log(key);
+            const constantNode = LiteGraph.createNode(key);
+            constantNode.pos = [x, y];
+            graph.add(constantNode);
+            
+            x += deltaX; // Increment y position for next node
+            if (x > window.innerWidth + 300) {
+                x = 50;
+                y += 150; // Move to the next column
+            }
+        } catch {
+            console.error(`can't load ${key}`);
         }
         continue;
         let node = LiteGraph.createNode(key);
