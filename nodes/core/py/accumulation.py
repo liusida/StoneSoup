de6e330788f6476c90cb203bf9beacf6bc5de320
Sida Liu <learner.sida.liu@gmail.com>
2024-07-13 13:29:22 +0800

    Accumulation node

---
+++ nodes/core/py/accumulation.py
@@ -0,0 +1,30 @@
+class NodeAccumulation:
+    title = "Server-side Accumulation"
+    desc = "Start from 0, add 1 each time, return the sum."
+
+    def __init__(self):
+        self.value = 0
+    
+    @classmethod
+    def INPUTS(s):
+        return [
+        ]
+    
+    @classmethod
+    def WIDGETS(s):
+        return [
+            # type, name, value, callback, options
+            {"type": "number", "name": "inc", "value": 1}
+        ]
+
+    @classmethod
+    def OUTPUTS(s):
+        return [
+            {"name": "output", "type": "INT"}
+        ]
+    
+    def main(self, inc):
+        current_value = self.value
+        self.value += inc
+        print("main function called.")
+        return (current_value,)
