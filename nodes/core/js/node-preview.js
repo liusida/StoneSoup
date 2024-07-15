786b771632088b2b1e8286c5adb83a434ab19d55
Sida Liu <learner.sida.liu@gmail.com>
2024-07-15 17:15:39 +0800

    add output trigger for preview node

--- nodes/core/js/node-preview.js
+++ nodes/core/js/node-preview.js
@@ -12,13 +12,14 @@ export class NodePreviewImage {
         this.image_url = null; // Store the URL of the uploaded image
         this.shape = "card";
         this.addInput("onTrigger", LiteGraph.EVENT);
+        this.addOutput("onTrigger", LiteGraph.EVENT);
         this.addInput("image", "image");
 
         this.widget_height = 60;
         this.chunks = [];
     }
 
-    onAction() {
+    async onAction() {
         const image_pointer = this.getInputData(1);
         if (image_pointer) {
             if (!this.img) {
@@ -26,64 +27,54 @@ export class NodePreviewImage {
                 this.img.width = this.imageWidth;
                 this.img.height = this.imageHeight;
             }
-            
-            // Send a POST request to the server to fetch the image details and stream
-            fetch(`${server_url}/preview`, {
+
+            const response = await fetch(`${server_url}/preview`, {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
                 },
                 body: JSON.stringify({ image_pointer: image_pointer }),
-            })
-                .then((response) => {
-                    // Extract image dimensions from headers
-                    this.imageWidth = parseInt(
-                        response.headers.get("X-Image-Width")
-                    );
-                    this.imageHeight = parseInt(
-                        response.headers.get("X-Image-Height")
-                    );
+            });
 
-                    return response.body;
-                })
-                .then((body) => {
-                    const reader = body.getReader();
-                    this.chunks = [];
-                    const updateInterval = 1000; // Time interval in milliseconds
+            // Extract image dimensions from headers
+            this.imageWidth = parseInt(response.headers.get("X-Image-Width"));
+            this.imageHeight = parseInt(response.headers.get("X-Image-Height"));
 
-                    function updateImage() {
-                        if (this.chunks.length > 0) {
-                            const blob = new Blob(this.chunks, { type: "image/png" });
-                            const imageUrl = URL.createObjectURL(blob);
-                            
-                            // Pre-load the image to avoid blinking
-                            const tempImg = new Image();
-                            tempImg.onload = () => {
-                                this.img.src = imageUrl;
-                                this.setDirtyCanvas(true);
-                            };
-                            tempImg.src = imageUrl;
-                        }
-                    }
-                    const intervalId = setInterval(updateImage.bind(this), updateInterval);
-                    
-                    reader.read().then(
-                        function process({ done, value }) {
-                            if (done) {
-                                clearInterval(intervalId); // Clear the interval when done
-                                updateImage.call(this); // Final update
-                                return;
-                            }
-                    
-                            this.chunks.push(value);
-                    
-                            reader.read().then(process.bind(this));
-                        }.bind(this)
-                    );
-                })
-                .catch((error) => {
-                    console.error("Error:", error);
-                });
+            const body = response.body;
+            const reader = body.getReader();
+            this.chunks = [];
+            const updateInterval = 1000; // Time interval in milliseconds
+
+            const updateImage = () => {
+                if (this.chunks.length > 0) {
+                    const blob = new Blob(this.chunks, { type: "image/png" });
+                    const imageUrl = URL.createObjectURL(blob);
+
+                    // Pre-load the image to avoid blinking
+                    const tempImg = new Image();
+                    tempImg.onload = () => {
+                        this.img.src = imageUrl;
+                        this.setDirtyCanvas(true);
+                    };
+                    tempImg.src = imageUrl;
+                }
+            };
+
+            const intervalId = setInterval(updateImage.bind(this), updateInterval);
+
+            const process = async ({ done, value }) => {
+                if (done) {
+                    clearInterval(intervalId); // Clear the interval when done
+                    updateImage.call(this); // Final update
+                    this.triggerSlot(0); // Trigger the next node
+                    return;
+                }
+
+                this.chunks.push(value);
+                reader.read().then(process.bind(this));
+            };
+
+            reader.read().then(process.bind(this));
         }
     }
 
