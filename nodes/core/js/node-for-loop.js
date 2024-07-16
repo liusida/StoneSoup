34c8397b3ae735e3d6f6b8731cde1f4d089cbe4d
Sida Liu <learner.sida.liu@gmail.com>
2024-07-16 12:25:01 +0800

    for loop with next node

--- nodes/core/js/node-for-loop.js
+++ nodes/core/js/node-for-loop.js
@@ -6,13 +6,14 @@ class NodeForLoop {
         this.title = "ForLoop";
         this.addInput("onTrigger", LiteGraph.EVENT);
         this.addInput("Break", LiteGraph.EVENT);
+        this.addOutput("Next", "ForLoopID");
         this.addOutput("Loop Body", LiteGraph.EVENT);
         this.addOutput("Completed", LiteGraph.EVENT);
         this.addOutput("Index", "number", "0");
         this.addWidget("number", "First Index", 1).options.step = 10;
-        this.addWidget("number", "Last Index", 10).options.step = 10;
+        this.addWidget("number", "Last Index", 3).options.step = 10;
         this.addWidget("number", "Step", 1).options.step = 10;
-        this.i = 0;
+        this.index = 0;
     }
 
     getWidgetValueByName(widgetName) {
@@ -26,17 +27,47 @@ class NodeForLoop {
         }
         return null;
     }
+
+    step() {
+        if (this.index<=this.getWidgetValueByName("Last Index")) {
+            this.triggerSlot(1);
+        } else {
+            this.triggerSlot(2);
+        }
+        this.index++;
+    }
     
     async onAction(action, param, options, action_slot) {
-        if (action_slot==0) { // for i=0
-            this.i = 0;
-        } else { // i++
-            this.i ++;
+        this.index = this.getWidgetValueByName("First Index");
+        // find the Next node and add a callback
+        var that=this;
+        var next = this.getOutputNodes(0);
+        if (next==null || next.length<=0) {
+            graphcanvas.createDialog("Please connect to the ForLoopNext Node");
+            return;
         }
-        this.setOutputData("i", this.i);
-        if (this.i<this.getWidgetValueByName('n')) {
-            this.triggerSlot(0);
+        next.forEach(node => {
+            node.stepFunction = () => {
+                that.step();
+            }
+        });
+        that.step();
+    }
+}
+class NodeForLoopNext {
+    constructor() {
+        this.title = "Next";
+        this.addInput("ForLoop", "ForLoopID");
+        this.addInput("Next", LiteGraph.EVENT);
+        this.stepFunction = null;
+    }
+    async onAction(action, param, options, action_slot) {
+        if (this.stepFunction) {
+            this.stepFunction.call();
+        } else {
+            graphcanvas.createDialog("Please connect to the ForLoop Node");
         }
     }
 }
 LiteGraph.registerNodeType("core/ForLoop", NodeForLoop);
+LiteGraph.registerNodeType("core/ForLoopNext", NodeForLoopNext);
