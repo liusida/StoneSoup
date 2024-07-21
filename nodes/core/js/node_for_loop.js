bdc5ebef7d72300bebc09d579a213c9f2965663b
Sida Liu <learner.sida.liu@gmail.com>
2024-07-22 07:44:30 +0800

    update loop and no need for that unpleasent vertical node

--- nodes/core/js/node_for_loop.js
+++ nodes/core/js/node_for_loop.js
@@ -1,14 +1,17 @@
-class NodeForLoop {
+import { NodeTemplate } from "/system/default/js/node.js";
+
+class NodeForLoop extends NodeTemplate{
     constructor() {
+        super();
         this.title = "ForLoop";
         this.addInput("onTrigger", LiteGraph.EVENT);
         this.addOutput("Next", "ForLoopID");
         this.addOutput("Loop Body", LiteGraph.EVENT);
         this.addOutput("Completed", LiteGraph.EVENT);
         this.addOutput("Index", "number", "0");
-        this.addWidget("number", "First Index", 1, this.onWidgetChanged).options.step = 10;
-        this.addWidget("number", "Last Index", 3, this.onWidgetChanged).options.step = 10;
-        this.addWidget("number", "Step", 1, this.onWidgetChanged).options.step = 10;
+        this.addWidget("number", "First Index", 1).options.step = 10;
+        this.addWidget("number", "Last Index", 3).options.step = 10;
+        this.addWidget("number", "Step", 1).options.step = 10;
         this.index = 0;
     }
 
@@ -35,6 +38,7 @@ class NodeForLoop {
     }
     
     async onAction(action, param, options, action_slot) {
+        await super.onAction(action, param, options, action_slot);
         this.index = this.getWidgetValueByName("First Index");
         // find the Next node and add a callback
         var that=this;
