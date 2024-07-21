4d1ee1e1902f6611b4de9dae22b299782d249a1d
Sida Liu <learner.sida.liu@gmail.com>
2024-07-21 08:44:01 +0800

    coloring the log

--- server/py/custom_logs.py
+++ server/py/custom_logs.py
@@ -1,16 +1,18 @@
 import logging
 from colorama import Fore, Style
+
 class ColorFormatter(logging.Formatter):
     def format(self, record):
+        # Prepend [ComfyUI] to the log message
         if record.name == 'root':
-            record.msg = f"{Fore.LIGHTBLUE_EX}{record.msg}{Style.RESET_ALL}"
+            # Since I don't want to modify ComfyUI's logging behavior (`logging.info`), make sure we don't use `root` and leave it to ComfyUI
+            record.msg = f"{Fore.BLUE}{Style.DIM}[ComfyUI]\t{record.msg}{Style.RESET_ALL}"
         elif record.name == 'server':
-            record.msg = f"{Fore.LIGHTGREEN_EX}{record.msg}{Style.RESET_ALL}"
-        elif record.name.startswith('watchfiles'):
-            record.msg = f"{Fore.LIGHTBLACK_EX}{record.msg}{Style.RESET_ALL}"
+            record.msg = f"{Fore.GREEN}{Style.NORMAL}[StoneSoup]\t{record.msg}{Style.RESET_ALL}"
         return super().format(record)
+
 # Configure logging
-formatter = ColorFormatter('%(levelname)s:%(name)s:\t%(message)s')
+formatter = ColorFormatter('%(levelname)s:\t%(message)s')
 handler = logging.StreamHandler()
 handler.setFormatter(formatter)
 logging.basicConfig(level=logging.INFO, handlers=[handler])
